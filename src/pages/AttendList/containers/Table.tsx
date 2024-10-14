1822066e5c2fb18348e6fd5c17355ceb20bc8bc7
CHOIYEJUN <dpwns108@gmail.com>
2024-10-15 00:41:07 +0900

    feat: 출석하기 기능개발
    - 출석 리스트 기능 개발중
    
    #dev

--- src/pages/AttendList/containers/Table.tsx
+++ src/pages/AttendList/containers/Table.tsx
@@ -1,31 +1,76 @@
-import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import { getAuth } from 'firebase/auth';
+
+import { Box, Button, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 
 import { IAttendDetailDatas } from '@/types/attend/attendType';
 
 const AttendListTable = (props: { data: IAttendDetailDatas[] }) => {
   const { data } = props;
 
+  // uid 를 가져와야해
+  const auth = getAuth();
+  const loggedInUid = auth.currentUser ? auth.currentUser.uid : null;
+
+  const myAttendance = data.find((item) => item.uid === loggedInUid);
+  const otherAttendance = data.filter((item) => item.uid !== loggedInUid);
+
+  const handleCancel = (uid: string) => {
+    console.log(uid);
+  };
   return (
-    <Table>
-      <Thead>
-        <Tr>
-          <Th>프로필 사진</Th>
-          <Th>이름(생년월일)</Th>
-          <Th>콕 제출 여부</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {data.map((item) => (
-          <Tr key={item.uid}>
-            <Td>
-              <img src={''} alt={'Profile'} width={'50'} height={'50'} />
-            </Td>
-            <Td>{`${item.userName}`}</Td>
-            <Td></Td>
+    <Box>
+      {/* 본인 출석 정보 표시 */}
+      {myAttendance && (
+        <Box mb={4}>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>프로필 사진</Th>
+                <Th>이름(생년월일)</Th>
+                <Th>콕 제출 여부</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr key={myAttendance.uid}>
+                <Td>
+                  <img src={''} alt={'Profile'} width={'50'} height={'50'} />
+                </Td>
+                <Td>{`${myAttendance.userName}`}</Td>
+                <Td>
+                  <Button colorScheme={'red'} size={'sm'} onClick={() => handleCancel(myAttendance.uid)}>
+                    출석 취소하기
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      )}
+
+      {/* 나머지 출석 정보 표시 */}
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>프로필 사진</Th>
+            <Th>이름(생년월일)</Th>
+            <Th>콕 제출 여부</Th>
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {otherAttendance.map((item) => (
+            <Tr key={item.uid}>
+              <Td>
+                <img src={''} alt={'Profile'} width={'50'} height={'50'} />
+              </Td>
+              <Td>{`${item.userName}`}</Td>
+              <Td>
+                <span>콕 제출 완료</span>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
   );
 };
 
